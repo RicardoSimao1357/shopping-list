00f72a64a9f625b3367c8e2c693e4d5e092734da
Ricardo Simão <ricardo.simao.1357@gmail.com>
2023-04-07 13:51:28 +0100

    Remove e clear items

--- script.js
+++ script.js
@@ -1,42 +1,58 @@
-const itemForm = document.getElementById("item-form");
-const itemInput = document.getElementById("item-input");
-const itemList = document.getElementById("item-list");
+const itemForm = document.getElementById('item-form');
+const itemInput = document.getElementById('item-input');
+const itemList = document.getElementById('item-list');
+const clearBtn = document.getElementById('clear');
 
 function addItem(e) {
   e.preventDefault();
 
   const newItem = itemInput.value;
+
   // Validate Input
-  if (newItem.value === "") {
-    alert("Please add an item");
+  if (newItem === '') {
+    alert('Please add an item');
     return;
   }
 
   //  Create list Item
-  const li = document.createElement("li");
+  const li = document.createElement('li');
   li.appendChild(document.createTextNode(newItem));
 
-  const button = createButton("remove-item btn-link text-red");
+  const button = createButton('remove-item btn-link text-red');
   li.appendChild(button);
 
   itemList.appendChild(li);
 
-  itemInput.value = "";
+  itemInput.value = '';
 }
 
 function createButton(classes) {
-  const button = document.createElement("button");
+  const button = document.createElement('button');
   button.className = classes;
-  const icon = createIcon("fa-solid fa-xmark");
+  const icon = createIcon('fa-solid fa-xmark');
   button.appendChild(icon);
   return button;
 }
 
 function createIcon(classes) {
-  const icon = document.createElement("i");
+  const icon = document.createElement('i');
   icon.className = classes;
   return icon;
 }
 
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains('remove-item')) {
+    e.target.parentElement.parentElement.remove();
+  }
+}
+
+function clearItems() {
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+}
+
 // Event Listeners
-itemForm.addEventListener("submit", addItem);
+itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearBtn.addEventListener('click', clearItems);
